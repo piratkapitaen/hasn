4de0ed51b7779d4ed2958056b17da7acbe46d946
pirat kapitaen <132739318+piratkapitaen@users.noreply.github.com>
2025-04-13 00:48:57 +0200

    Add files via upload

--- sphere_mve.py
+++ sphere_mve.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pyvista as pv
+
 from stpyvista import stpyvista
+from stpyvista.utils import is_the_app_embedded, start_xvfb
+import pantry.stpyvista_pantry as stpv
+from pantry.webapp_fragments import (
+    gallery,
+    fill_up_main_window,
+    fill_install_instructions,
+)
+
+start_xvfb()
+st.session_state.is_app_embedded = st.session_state.get("is_app_embedded", is_the_app_embedded())
 
 res = st.slider("Resolution", 5, 100, 20, 5)
 
