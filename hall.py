3f5656845d6cb3948516e9b029a61f1b7fef0ba4
pirat kapitaen <132739318+piratkapitaen@users.noreply.github.com>
2025-04-13 11:49:10 +0200

    Add files via upload

--- hall.py
+++ hall.py
@@ -99,11 +99,137 @@ def hys_speed_direction(signal):
 def sphere():
     return pv.Sphere(radius=1.0, center=(0, 0, 0))
 
+def hys_speed_dir(signal1, signal2):
+    output = np.zeros_like(signal1)
+    state = 0
+    for i, (val1, val2) in enumerate(zip(signal1, signal2)):
+        if i < 600:
+            output[i] = 0
+        elif state == 0 and val1 > .009 and val2 < 0.009:
+            state = 10
+        elif state == 0 and val2 > .009 and val1 > 0.009 :
+            state = 10
+        output[i] = state
+    return output
+
+def generate_memory():
+    if mode=='speed/speed':
+        y_sin_hyst = hysterese(y_sin, int(1. * float(threshold)), -1.*int(1. * float(threshold)))
+        y_cos_hyst = hysterese(y_cos, int(1. * float(threshold)), -1.*int(1. * float(threshold)))
+    else:
+        y_sin_hyst = hysterese(y_sin, int(1. * float(threshold)), -1.*int(1. * float(threshold)))
+        y_cos_hyst = hysterese(y_cos, int(1. * float(threshold)), -1.*int(1. * float(threshold)))
+        y_cos_hyst = hys_speed_dir(y_sin_hyst, y_cos_hyst)
+        
+    memory = []
+    # 12 Zeilen mit 4 Hexadezimalzahlen pro Zeile
+    print('Werte: ', int(bw), float(threshold), str(mode) ) # threshold*10 in 0.1mT
+    bon = int(10 * float(threshold))
+    bandwidth = int(bw)
+    res = 0
+    if axis=='XY':
+        pass
+    elif axis=='ZX':
+        res = res | 1
+    elif axis=='ZY':
+        res = res | 2
+    memory.append(res);     res = 0;
+
+    if mode=='speed/speed':
+        res = res | 8
+    else:
+        res = 0
+    # TC = 0ppm/K
+    if int(bw)==5:
+        res = res | 6
+    elif int(bw)==10:
+        res = res | 4
+    elif int(bw)==20:
+        res = res | 2
+    elif int(bw)==40:
+        res = res | 0
+    memory.append(res);     res = 0;
+    memory.append(0);       res = 0;
+
+    if fusi!='disabled':
+        res = res | 2
+    if poweron!='low':
+        res = res | 1
+    memory.append(res);     res = 0;
+    
+    memory.append(256 - bon) #  Two's complement
+    memory.append(bon)
+    memory.append(256 - bon) #  Two's complement
+    memory.append(bon)
+    if UID=='0x00000000':
+        memory.append(0)
+        memory.append(0)
+        memory.append(0)
+        memory.append(0)
+    else:
+        memory.append(85)
+        memory.append(170)
+        memory.append(85)
+        memory.append(170)
+
+    memory_masked = []
+
+    # iterate over EEPROM #0 - #7
+    for mm, bitmask in zip(memory[:-4], [239, 254, 255, 11, 255, 255, 255, 255]):
+        memory_masked.append(mm & bitmask)
+
+    # calculate parity
+    parity = calc_parity(memory_masked)
+    # correct parity in memory
+    memory[1] = memory[1] | parity
+
+
+# + '     0b'+format(byte, '08b')
+    my_str = ""
+    arr = '['
+    for byte in memory:
+        my_str += str(byte)+ '     0b'+format(byte, '08b')+ ' \n'
+        arr += str(byte) + ' '
+    mem = bytearray(memory)
+    arr += ']\n'
+
+    hex_string = mem.hex()
+    # Verbinden der Zeilen mit Zeilenumbrüchen
+    st.session_state.text = 'EEPROM bytes:   '+arr+ 'HEX String:    ' + hex_string + '\n' + 'Parity: ' + str(parity)+'\n'+my_str
+
+    # Channel A and B plots
+    ax1.plot(x_plot, y_sin, label='Channel A', alpha=0.4)
+    ax1.plot(x_plot, y_sin_hyst, label='OUT1', color='red')
+    ax1.axhline(int(1. * float(threshold)), color='green', linestyle='--', label='BON')
+    ax1.axhline(-1.*int(1. * float(threshold)), color='orange', linestyle='--', label='BOFF')
+    ax1.set_ylabel('Channel A')
+#    ax1.set_title('Channel A')
+    ax1.legend()
+    ax1.grid(True)
+    ax2.plot(x_plot, y_cos, label='Channel B', alpha=0.4)
+    ax2.plot(x_plot, y_cos_hyst, label='OUT2', color='blue')
+    ax2.axhline(int(1. * float(threshold)), color='green', linestyle='--', label='BON')
+    ax2.axhline(-1.*int(1. * float(threshold)), color='orange', linestyle='--', label='BOFF')
+    ax2.set_ylabel('Channel B')
+    ax2.set_xlabel('x')
+#    ax2.set_title('Channel B')
+    ax2.legend()
+    ax2.grid(True)
+
+    if 'fig' in st.session_state:
+        st.pyplot(st.session_state['fig'])
+
+# Sidebar
+with st.sidebar:
+#    st.title("EEPROM")    
+    st.markdown('''
+    EEPROM Config generator  
+    ''', unsafe_allow_html=True)
+    st.image('static/ic_icon.png', width=100)
 
 
 
-
-
+    
 res = st.slider("Resolution", 5, 100, 20, 5)
 
 # Set up plotter
